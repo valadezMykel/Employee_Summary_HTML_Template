512464f019b9269043b9421841d2c40fc036d653
Mykel Valadez <valadezmykel@gmail.com>
2020-11-06 11:43:56 -0800

    switch statement added to app.js to sort employee types

--- Develop/app.js
+++ Develop/app.js
@@ -5,60 +5,87 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+let employeeArr = [];
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-inquirer.prompt([
-    {
-        message: "This program will no create an HTML containing information on employees.  Please enter in the information of each employee one at a time as prompted."
-    },
-    {
-        type: "list",
-        name: "role",
-        message: "Which role does this employee have?",
-        choices: [
-            "Engineer",
-            "Intern",
-            "Manager"
-        ]
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "Input the employee's name"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Input employee id"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Input the employee's email"
-    },
-    {
-        type: "input",
-        name: "gitHubUserName",
-        message: "Input engineer's github user name",
-        when: (inputs) => inputs.role === "Engineer"
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "Input intern's school name",
-        when: (inputs) => inputs.role === "Intern"
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "Input manager's office number",
-        when: (inputs) => inputs.role === "Manager"
-    }
-])
+function askQuestions(){
+    inquirer.prompt([
+        {
+            name: "prompt",
+            message: "This program will no create an HTML containing information on employees.  Please enter in the information of each employee one at a time as prompted."
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "Which role does this employee have?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Manager"
+            ]
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Input the employee's name"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Input employee id"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Input the employee's email"
+        },
+        {
+            type: "input",
+            name: "gitHubUserName",
+            message: "Input engineer's github user name",
+            when: (inputs) => inputs.role === "Engineer"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Input intern's school name",
+            when: (inputs) => inputs.role === "Intern"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Input manager's office number",
+            when: (inputs) => inputs.role === "Manager"
+        },
+        {
+            type: "confirm",
+            name: "again",
+            message: "Would you like add another employee?"
+        }
+    ]).then((inputs)=>{
 
+        switch(inputs.role){
+            case "Engineer":
+                employeeArr.push(new Engineer(inputs.name, inputs.id, inputs.email, inputs.gitHubUserName));
+                break;
+            case "Intern":
+                employeeArr.push(new Intern(inputs.name, inputs.id, inputs.email, inputs.school));
+                break;
+            case "Manager":
+                const newEMp
+        };
+        const mykel = new [`${inputs.role}`](inputs.name, inputs.id, inputs.email, inputs.officeNumber);
+        if(inputs.again){
+            askQuestions();
+        };
+
+    
+    })
+};
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
